5b6c9361a7ed58ce4c12e9ec7d1086deaf1a871b
Alaeddine_Rekik <alaa.eddine.rekik@gmail.com>
2023-10-10 17:42:43 +0100

    Creating Tower Class

---
+++ Source/TowerDefense/Building/Tower.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BuildingBase.h"
+#include "Tower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENSE_API ATower : public ABuildingBase
+{
+	GENERATED_BODY()
+	
+};
