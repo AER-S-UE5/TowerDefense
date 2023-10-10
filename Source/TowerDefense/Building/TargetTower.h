e20990b36692ed32730af4a07750f07ee6808899
Alaeddine_Rekik <alaa.eddine.rekik@gmail.com>
2023-10-10 18:47:08 +0100

    Creating TargetTower

---
+++ Source/TowerDefense/Building/TargetTower.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tower.h"
+#include "TargetTower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENSE_API ATargetTower : public ATower
+{
+	GENERATED_BODY()
+	
+};
