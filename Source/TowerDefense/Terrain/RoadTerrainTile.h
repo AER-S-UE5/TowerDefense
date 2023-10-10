3d3ac4300428aff3b02a4c7e27a11293a653dcd2
Alaeddine_Rekik <alaa.eddine.rekik@gmail.com>
2023-10-10 16:41:01 +0100

    Creating RoadTerrainTile : 1/ Creating the C++ class for the RoadTerrainTile 2/ Creating the Blueprint class of the RoadTerrainTile 3/ Placing the RoadTerrainTiles in the level and adding a plane floor

---
+++ Source/TowerDefense/Terrain/RoadTerrainTile.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TerrainTilesBase.h"
+#include "RoadTerrainTile.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENSE_API ARoadTerrainTile : public ATerrainTilesBase
+{
+	GENERATED_BODY()
+	
+};
