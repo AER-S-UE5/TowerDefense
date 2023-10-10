8547d6e5cfaf06b72b2e50dad64a7c9b9a22de97
Alaeddine_Rekik <alaa.eddine.rekik@gmail.com>
2023-10-10 17:38:29 +0100

    Creating Building Class

---
+++ Source/TowerDefense/Building/BuildingBase.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BuildingBase.generated.h"
+
+UCLASS()
+class TOWERDEFENSE_API ABuildingBase : public AActor
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* DefaultSceneComponent;
+	
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* BuildingMesh;
+
+
+	
+public:	
+	// Sets default values for this actor's properties
+	ABuildingBase();
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
